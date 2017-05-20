4c0116904db2c4299464c89ed76406adcaaa1a52
Jesse Fender <jf0772256@otc.edu>
2017-05-20 14:02:58 -0500

    i went ahead and added Database.php code to API

--- dev/class/API.php
+++ dev/class/API.php
@@ -1,6 +1,30 @@
 <?php
 
-include('./class/Database.php');
+class Database
+{
+  private $host;
+  private $user;
+  private $password;
+  private $dbName;
+  private $dbC;
+  function __construct($host, $user, $password, $dbName){
+    $this->host = $host;
+    $this->user = $user;
+    $this->password = $password;
+    $this->dbName = $dbName;
+    @$this->dbC = mysqli_connect($this->host,$this->user,$this->password,$this->dbName);
+
+    if (!$this->dbC) {
+      die("We were not able to connect to the database " . $this->dbName . "@" . $this->host . " with the given crudentials. Please verify and check all information and retry.");
+    }
+  }
+  //getters
+  protected function getDBConnection(){return $this->dbC;}
+  //check connection
+  public function checkDBC(){return @mysqli_ping($this->dbC);}
+  //close the current connection
+  protected function dbCClose($connection){return mysqli_close($connection);}
+}
 
 /**
  * API File generator and reader
